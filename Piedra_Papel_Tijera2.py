676c568b539407a4866b59f6b5a46011d1d977b5
Emimarquez06 <106921961+Emimarquez06@users.noreply.github.com>
2022-08-29 19:54:44 -0300

    Add files via upload

--- Piedra_Papel_Tijera2.py
+++ Piedra_Papel_Tijera2.py
@@ -1,23 +1,14 @@
 import random
-from time import sleep
+bandera = True
 
-print ('Bienvenido, vamos a jugar a Piedra, papel o tijera.')
-print ('')
-sleep(2)
-print ('Jugamos al mejor de tres, o prefieres cambiar?')
-sleep(1)
-print ('')
-
-#Funcion que realiza la lógica del juego
-def juego(intentos):
- x = 0
- tu = 0
- pc = 0
- while str(x) != intentos:
-  aleatorio = random.randrange(0, 3)
-    elige_pc = ''
-
-    print ('')
+def juego():
+    x = 0
+    elige_usuario = 0
+    elige_pc = 0
+    aleatorio = random.randrange(0, 3)
+    elige_pc = '' 
+    
+    print('')
     print('1)Piedra')
     print('2)Papel')
     print('3)Tijera')
@@ -25,44 +16,72 @@ def juego(intentos):
 
     opcion = int(input('Elige una opción: '))
 
-    if opcion == 1:
-        elige_usuario = 'Piedra'
-    elif opcion == 2:
-        elige_usuario = 'Papel'
-    elif opcion == 3:
-        elige_usuario = 'Tijera'
+    elige_usuario = usuario(opcion)
+    elige_pc = pc(aleatorio)
+
+    if elige_usuario == 'Error':
+        print ('Numero Invalido')
+    elif elige_usuario == elige_pc:
+            print('')
+            print('Elegiste: ', elige_usuario)
+            print('PC eligió: ', elige_pc)
+            print('Empate')
+    elif elige_usuario == 'Tijera':
+            if elige_pc == 'Piedra':
+                print('')
+                print('Elegiste: ', elige_usuario)
+                print('PC eligió: ', elige_pc)
+                print('Perdiste')   
+            else:
+                print('')
+                print('Elegiste: ', elige_usuario)
+                print('PC eligió: ', elige_pc)
+                print('Ganaste')
+    elif elige_usuario == 'Piedra':
+            if elige_pc == 'Tijera':
+                print('')
+                print('Elegiste: ', elige_usuario)
+                print('PC eligió: ', elige_pc)
+                print('Ganaste')
+            else:
+                print('')
+                print('Elegiste: ', elige_usuario)
+                print('PC eligió: ', elige_pc)
+                print('Perdiste')
+    elif elige_usuario == 'Papel':
+            if elige_pc == 'Tijera':
+                print('')
+                print('Elegiste: ', elige_usuario)
+                print('PC eligió: ', elige_pc)
+                print('Perdiste')
+            else:
+                print('')
+                print('Elegiste: ', elige_usuario)
+                print('PC eligió: ', elige_pc)
+                print('Ganaste')
+    
+
+def usuario(x: str):
+    if x == 1:
+        return 'Piedra'
+    elif x == 2:
+        return 'Papel'
+    elif x == 3:
+        return 'Tijera'
+    elif x == 4:
+        bandera = False
+        return ''
     else:
-        break
+        return 'Error'
 
-    print('Elegiste: ', elige_usuario)
+def pc (x: str):
+    if x == 0:
+            return  'Piedra'
+    elif x == 1:
+            return 'Papel'
+    elif x == 2:
+            return 'Tijera'
 
-    if aleatorio == 0:
-        elige_pc = 'Piedra'
-    elif aleatorio == 1:
-        elige_pc = 'Papel'
-    elif aleatorio == 2:
-        elige_pc = 'Tijera'
-
-    print('PC eligió: ', elige_pc)
-    print('...')
-
-    if elige_pc == 'Piedra' and elige_usuario == 'Papel':
-        print('Ganaste, Papel envuelve a Piedra')
-
-    elif elige_pc == 'Papel' and elige_usuario == 'Tijera':
-        print("Ganaste, Tijera corta Papel")
-
-    elif elige_pc == 'Tijera' and elige_usuario == 'Piedra':
-        print('Ganaste, Piedra aplasta Tijera')
-
-    if elige_pc == 'Papel' and elige_usuario == 'Piedra':
-        print('Perdiste, Papel envuelve Piedra')
-
-    elif elige_pc == 'Tijera' and elige_usuario == 'Papel':
-        print('Perdiste, Tijera corta Papel')
-
-    elif elige_pc == 'Piedra' and elige_usuario == 'Tijera':
-        print('Perdiste, Piedra aplasta Tijera')
-
-    elif elige_pc == elige_usuario:
-        print('Empate')
+print ('Bienvenido, vamos a jugar a Piedra, papel o tijera.')
+while bandera:
+    juego()
